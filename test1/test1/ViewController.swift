f2b69ae668fee64bbc009f55f3247fe828f76a7e
Your Name <you@example.com>
2018-11-08 22:38:57 +0300

    first comm

---
+++ test1/test1/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Владимир Уланов on 08.11.2018.
+//  Copyright © 2018 UlanovApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let ggg = 5
+        print(ggg)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
